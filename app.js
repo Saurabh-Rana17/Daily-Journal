5d476f82d953f4ac7041247e37675ef8f740eaa5
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-07-23 22:51:06 +0530

    c4

--- app.js
+++ app.js
@@ -17,7 +17,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
+  res.render("home", { homeContent: homeStartingContent });
+});
+app.get("/contact", function (req, res) {
+  res.render("home", { homeContent: homeStartingContent });
+});
+app.get("/about", function (req, res) {
   res.render("home", { homeContent: homeStartingContent });
 });
 
