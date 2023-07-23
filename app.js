c0de36a72a9596daeff948f42788a77c520d9e7e
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-07-23 22:35:35 +0530

    c2

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 app.get("/", function (req, res) {
-  res.render("home");
+  res.render("home", { homeContent: homeStartingContent });
 });
 
 app.listen(3000, function () {
