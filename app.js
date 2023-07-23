45d85085437f485fa2c39c60b1b8d7dcfa4ad5e1
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-07-24 00:20:36 +0530

    c11

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+let posts = [];
+
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -20,6 +22,7 @@ app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   res.render("home", { homeContent: homeStartingContent });
+  console.log(posts);
 });
 app.get("/contact", function (req, res) {
   res.render("contact", { contactContent: contactContent });
@@ -32,7 +35,8 @@ app.get("/compose", function (req, res) {
 });
 app.post("/compose", function (req, res) {
   const composeData = { title: req.body.i1, body: req.body.tarea };
-  console.log(composeData);
+  posts.push(composeData);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
